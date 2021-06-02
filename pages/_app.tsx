867b389dbeabd3dfc2ee38cceb5b7d8cf7ce105d
Wilgnne Khawan <wilgnne.kba@gmail.com>
2021-06-02 00:45:13 +0000

    feat: Use ts in _document

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,19 @@
 import { ChakraProvider } from "@chakra-ui/react"
+import { Head } from "next/document"
 
 import "../styles/global.css"
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
-    )
+    <>
+      <Head>
+        <title>Wilgnne K.</title>
+      </Head>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
+  )
 }
 
 export default MyApp
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,16 +1,16 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { DocumentContext, Html, Head, Main, NextScript } from 'next/document'
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx) {
+  static async getInitialProps(ctx: DocumentContext) {
     const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
+
+    return initialProps
   }
 
   render() {
     return (
       <Html style={{ height: "100%" }}>
         <Head>
-          <title>Wilgnne K.</title>
           <meta name="description" content="Wilgnne K." />
           <link rel="icon" href={'/favicon.ico'} />
         </Head>
